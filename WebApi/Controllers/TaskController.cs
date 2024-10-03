c91ca5f4d1e47b34b1c695aff32ae1cd5cac642a
Aleena-Siby <aleenasiby92002@gmail.com>
2024-10-03 18:33:52 +0530

    Initial commit

---
+++ WebApi/Controllers/TaskController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyMvcApp.Models; // Import your models
+using Microsoft.EntityFrameworkCore;
+
+
+namespace WebApi.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")] 
+    public class TasksController : ControllerBase
+    {
+        
+    }
+}
